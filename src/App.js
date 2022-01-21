a3ec8ae0b1cb48b0bc8d969a692b35ca500f6dfc
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-21 21:36:20 +0100

    first example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
 
 import './App.css';
 
 const App = () => {
+  useEffect(
+    () => console.log(`Po wyrenderowaniu DOM`),
+    []
+  );
+
+  useLayoutEffect(
+    () => console.log(`Przed wyrenderowaniem DOM`),
+    []
+  );
+
+  console.log(`render`);
+
   return (
     <div>
       <h1>Lorem ipsum dolor</h1>
